638e2702ed12628ef181e075c77e828ef0734e72
Pradeep Peiris <pradeep_peiris@discovery.com>
2022-07-31 11:35:41 +0200

    fix code formatting and unit tests

---
+++ src/test/java/com/example/employee_system_new/controller/EmployeeControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.employee_system_new.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class EmployeeControllerTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllEmployees() {
+        ResponseEntity<List> respEntity = restTemplate.getForEntity("/api/v1/employees", List.class);
+        assertThat(respEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+}
